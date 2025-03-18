167a8361fea56d5443d55af21060769a4c905e12
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-03-18 09:49:31 +0300

    Fetch On Demand Lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('#searchByID').value;
+
+    fetch(`http://localhost:3000/movies/${input}`)
+        .then(res => res.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.textContent = data.title;
+            summary.textContent = data.summary;
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
